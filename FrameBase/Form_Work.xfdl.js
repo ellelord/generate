f5efe5ba5cc3d6369a9a0256cc488abadfdec8f2
ellelordPC2 <ellelord@tobesoft.com>
2018-11-02 11:32:39 +0900

    test1

--- FrameBase/Form_Work.xfdl.js
+++ FrameBase/Form_Work.xfdl.js
@@ -20,12 +20,12 @@
 
             
             // UI Components Initialize
-            obj = new Button("Button00","50","70","120","50",null,null,null,null,null,null,this);
+            obj = new Button("Button00","30","40","120","50",null,null,null,null,null,null,this);
             obj.set_taborder("0");
             obj.set_text("Button00");
             this.addChild(obj.name, obj);
 
-            obj = new Button("Button01","44","152","120","50",null,null,null,null,null,null,this);
+            obj = new Button("Button01","30","120","120","50",null,null,null,null,null,null,this);
             obj.set_taborder("1");
             obj.set_text("Button01");
             this.addChild(obj.name, obj);
